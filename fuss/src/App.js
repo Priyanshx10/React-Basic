6929e80fa487b6bcc68199df7c16ad8efb05a970
Priyansh <Priyanshyadav1012@gmail.com>
2024-06-28 16:08:51 +0530

    Todo Style

--- fuss/src/App.js
+++ fuss/src/App.js
@@ -6,7 +6,7 @@ import TodoItem from './components/TodoItem';
 function App() {
   return (
     <div className='bg-black h-screen w-screen flex justify-center items-center'>
-      <div className='w-[300px]  h-[320px] p-10 bg-white rounded-xl justify-between'>
+      <div className='w-[300px]  h-[320px] p-10 bg-white rounded-xl justify-between space-y-2'>
         <Header/>
         <TodoItem/> 
         <TodoItem/>
